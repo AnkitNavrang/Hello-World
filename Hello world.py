9c38b43313f2fd9f579f874b04a4f4ef984d53cf
AnkitNavrang <61693600+AnkitNavrang@users.noreply.github.com>
2021-01-05 07:14:41 +0530

    Add files via upload

---
+++ Hello world.py
@@ -0,0 +1,27 @@
+#code runs in python 3.4 or above
+
+#Disclaimer- All the vaiables  used in this program are randomly generated, any relevence to any person living or dead is completely coincidental 
+
+Helen = 'Not Helen Keller'
+loves =  'loves'
+word_porn = 'word porn'
+in_low_definition = 'low definition'
+in_really_low_definition = in_low_definition[0] + in_low_definition[4]
+
+
+#Parental advisory - only above 18 can call this function 
+def three_sum(one,another_one,yet_another_one):
+    
+    four_is_jelous = one + another_one + yet_another_one
+
+    print(four_is_jelous)
+
+
+What = Helen[4:7] + loves[:2]
+
+do_you = ' '
+
+expect = word_porn[:3] + in_really_low_definition
+
+#seriously
+three_sum(What,do_you,expect)
